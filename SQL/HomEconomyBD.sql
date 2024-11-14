5bf207f3f2033050bcbcceaf1c6426b3bb14a1ca
MatiMolina6404 <mati2004molina@gmail.com>
2024-11-14 16:39:52 -0300

    Subir archivo SQL
    Archivo correspondiente a la entrega 4 de seminario.

--- SQL/HomEconomyBD.sql
+++ SQL/HomEconomyBD.sql
@@ -23,11 +23,15 @@ DROP TABLE IF EXISTS `ahorros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ahorros` (
-  `idAhorro` int NOT NULL,
-  `cantidadAhorrada` int NOT NULL,
-  `ultimoMovimiento` date NOT NULL,
-  PRIMARY KEY (`idAhorro`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idAhorro` int NOT NULL AUTO_INCREMENT,
+  `concepto` varchar(45) NOT NULL,
+  `cantidad` int NOT NULL,
+  `fecha` date NOT NULL,
+  `idUsuarioAho` int NOT NULL,
+  PRIMARY KEY (`idAhorro`),
+  KEY `idUsuarioAho_idx` (`idUsuarioAho`),
+  CONSTRAINT `idUsuarioAho` FOREIGN KEY (`idUsuarioAho`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,12 +42,15 @@ DROP TABLE IF EXISTS `categorias`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categorias` (
-  `idCategoria` int NOT NULL,
+  `idCategoria` int NOT NULL AUTO_INCREMENT,
   `tipo` varchar(45) NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `descripcion` varchar(45) NOT NULL,
-  PRIMARY KEY (`idCategoria`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `idUsuarioCat` int NOT NULL,
+  PRIMARY KEY (`idCategoria`),
+  KEY `idUsuarioCat_idx` (`idUsuarioCat`),
+  CONSTRAINT `idUsuarioCat` FOREIGN KEY (`idUsuarioCat`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -54,14 +61,14 @@ DROP TABLE IF EXISTS `contactarequipo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contactarequipo` (
-  `idMensaje` int NOT NULL,
+  `idMensaje` int NOT NULL AUTO_INCREMENT,
   `idUsuarioM` int NOT NULL,
   `mensaje` varchar(45) NOT NULL,
   `fecha` date NOT NULL,
   PRIMARY KEY (`idMensaje`),
   KEY `idUsuarioM_idx` (`idUsuarioM`),
   CONSTRAINT `idUsuarioM` FOREIGN KEY (`idUsuarioM`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,14 +79,14 @@ DROP TABLE IF EXISTS `familia`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `familia` (
-  `idFamilia` int NOT NULL,
-  `idUsuario` int NOT NULL,
+  `idFamilia` int NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(45) NOT NULL,
   `Relacion` varchar(45) DEFAULT NULL,
+  `idUsuario` int NOT NULL,
   PRIMARY KEY (`idFamilia`),
   KEY `idUsuario_idx` (`idUsuario`),
-  CONSTRAINT `idUsuarioF` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -90,22 +97,21 @@ DROP TABLE IF EXISTS `gastos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `gastos` (
-  `idGasto` int NOT NULL,
   `idUsuarioG` int NOT NULL,
-  `idFamiliarG` int DEFAULT NULL,
-  `idCategoriaG` int DEFAULT NULL,
-  `cantidad` int NOT NULL,
+  `idGasto` int NOT NULL AUTO_INCREMENT,
   `fecha` date NOT NULL,
+  `cantidad` int NOT NULL,
+  `idCategoriaG` int DEFAULT NULL,
   `comentario` varchar(45) DEFAULT NULL,
+  `idFamiliarG` int DEFAULT NULL,
   `comprobante` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idGasto`),
   KEY `idFamiliar_idx` (`idFamiliarG`),
   KEY `idCategoria_idx` (`idCategoriaG`),
   KEY `idUsuario_idx` (`idUsuarioG`),
   CONSTRAINT `idCategoriaG` FOREIGN KEY (`idCategoriaG`) REFERENCES `categorias` (`idCategoria`),
-  CONSTRAINT `idFamiliarG` FOREIGN KEY (`idFamiliarG`) REFERENCES `familia` (`idFamilia`),
   CONSTRAINT `idUsuarioG` FOREIGN KEY (`idUsuarioG`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,12 +127,12 @@ CREATE TABLE `informes` (
   `fechaInicial` date NOT NULL,
   `fechaFinal` date NOT NULL,
   `idCategoria` int DEFAULT NULL,
-  `idFamiliar` int DEFAULT NULL,
+  `idFamilia` int DEFAULT NULL,
   PRIMARY KEY (`idInforme`),
-  KEY `idFamiliaInf_idx` (`idFamiliar`),
   KEY `idCategoriaInf_idx` (`idCategoria`),
-  CONSTRAINT `idCategoriaInf` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`),
-  CONSTRAINT `idFamiliaInf` FOREIGN KEY (`idFamiliar`) REFERENCES `familia` (`idFamilia`)
+  KEY `idFamiliar` (`idFamilia`),
+  CONSTRAINT `idCategoria` FOREIGN KEY (`idCategoria`) REFERENCES `categorias` (`idCategoria`),
+  CONSTRAINT `idFamiliar` FOREIGN KEY (`idFamilia`) REFERENCES `familia` (`idFamilia`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -138,22 +144,21 @@ DROP TABLE IF EXISTS `ingresos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingresos` (
-  `idIngreso` int NOT NULL,
   `idUsuarioI` int NOT NULL,
-  `idFamiliarI` int DEFAULT NULL,
-  `idCategoriaI` int DEFAULT NULL,
-  `cantidad` int NOT NULL,
+  `idIngreso` int NOT NULL AUTO_INCREMENT,
   `fecha` date NOT NULL,
+  `cantidad` int NOT NULL,
+  `idCategoriaI` int DEFAULT NULL,
   `comentario` varchar(45) DEFAULT NULL,
+  `idFamiliarI` int DEFAULT NULL,
   `comprobante` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idIngreso`),
   KEY `idUsuarioI_idx` (`idUsuarioI`),
   KEY `idFamiliar_idx` (`idFamiliarI`),
   KEY `idCategoria_idx` (`idCategoriaI`),
   CONSTRAINT `idCategoriaI` FOREIGN KEY (`idCategoriaI`) REFERENCES `categorias` (`idCategoria`),
-  CONSTRAINT `idFamiliarI` FOREIGN KEY (`idFamiliarI`) REFERENCES `familia` (`idFamilia`),
   CONSTRAINT `idUsuarioI` FOREIGN KEY (`idUsuarioI`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -187,7 +192,7 @@ DROP TABLE IF EXISTS `movimientosahorros`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `movimientosahorros` (
-  `idMovimiento` int NOT NULL,
+  `idMovimiento` int NOT NULL AUTO_INCREMENT,
   `idUsuarioMov` int NOT NULL,
   `idAhorro` int NOT NULL,
   `tipoMovimiento` varchar(45) NOT NULL,
@@ -198,7 +203,7 @@ CREATE TABLE `movimientosahorros` (
   KEY `idAhorro_idx` (`idAhorro`),
   CONSTRAINT `idAhorro` FOREIGN KEY (`idAhorro`) REFERENCES `ahorros` (`idAhorro`),
   CONSTRAINT `idUsuarioMov` FOREIGN KEY (`idUsuarioMov`) REFERENCES `usuario` (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,11 +214,12 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `idUsuario` int NOT NULL,
+  `idUsuario` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   `contraseña` varchar(45) NOT NULL,
+  `dineroInicial` int NOT NULL,
   PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -225,4 +231,4 @@ CREATE TABLE `usuario` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-27 19:42:08
+-- Dump completed on 2024-11-14 16:38:12
